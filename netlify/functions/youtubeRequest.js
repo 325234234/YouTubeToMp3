536df56174ba32a698c5d15677cbf960c34e8f97
Tino Baumgart <tn.bmgrt@gmail.com>
2023-07-25 19:05:19 +0200

    server function, css refactor, responive

--- netlify/functions/youtubeRequest.js
+++ netlify/functions/youtubeRequest.js
@@ -1,18 +1,15 @@
-const keys = {
-    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY    
-}
-
+//get a list object from the YouTube data V3 API that contains information on the queried video
 const handler = async (event) => {
-    try {
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=id&id=${event.body}&key=${keys.GOOGLE_API_KEY}`)
-        const data = await response.json()
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data),
-        }
-    } catch (error) {
-        return { statusCode: 500, body: error.toString() }
-    }
+  try {
+      const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=id&id=${event.body}&key=${process.env.GOOGLE_API_KEY}`)
+      const data = await response.json()
+      return {
+          statusCode: 200,
+          body: JSON.stringify(data),
+      }
+  } catch (error) {
+      return { statusCode: 500, body: error.toString() }
+  }
 }
 
 module.exports = { handler }

5ce449da34cc8584b8820ecc0b396a4f2f056cfd
Tino Baumgart <tn.bmgrt@gmail.com>
2023-07-25 14:31:22 +0200

    changed api calls to calls to serverelss functs

--- src/App.jsx
+++ src/App.jsx
@@ -42,21 +42,33 @@ export default function App() {
     //   setShowModal(true)    
     // }
 
-    
+    if(await videoExists(videoUrl)) {      
+      try {
+        setLoading(true)
+        setShowModal(true) 
 
-    const url = "https://musical-daffodil-6853f7.netlify.app/.netlify/functions/fetchRequests"
-    const options = {
-        method: 'POST',
-        headers: {
-            'content-type': 'text/plain',
-        },
-        body: "test"
+        const url = "https://musical-daffodil-6853f7.netlify.app/.netlify/functions/rapidRequest"
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'text/plain',
+            },
+            body: videoUrl
+        }
+        const response = await fetch(url, options)
+        const data = await response.json()
+        console.log(data)
+
+        startDownload(data)
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setLoading(false)
+        setVideoURL("")
+      }
+    } else {      
+      setShowModal(true)    
     }
-    const response = await fetch(url, options)
-    const data = await response.json()
-    console.log(data)
-
-
   }
 
   //initiate download by adding anchor tag with download attribute and clicking it
@@ -70,6 +82,23 @@ export default function App() {
   }
 
   async function videoExists(url) {
+      // //check if the url contains a valid YouTube video ID
+      // const regEx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/
+
+      // //if url contains a proper video ID, match returns an array with the ID at position 1
+      // if(url.match(regEx)) {
+      //   const id = url.match(regEx)[1]
+
+      //   //check if video exists
+      //   const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=id&id=${id}&key=${keys.env.GOOGLE_API_KEY}`)
+      //   const data = await response.json()
+
+      //   // 0 = no video = falsey = false, 1 = video = truthy = true
+      //   return data.items.length  
+      // } else {        
+      //   return false
+      // }    
+
       //check if the url contains a valid YouTube video ID
       const regEx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/|shorts\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/
 
@@ -78,14 +107,23 @@ export default function App() {
         const id = url.match(regEx)[1]
 
         //check if video exists
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=id&id=${id}&key=${keys.env.GOOGLE_API_KEY}`)
+        const url = "https://musical-daffodil-6853f7.netlify.app/.netlify/functions/youtubeRequest"
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'text/plain',
+            },
+            body: id
+        }
+        const response = await fetch(url, options)
         const data = await response.json()
+        console.log(data)
 
         // 0 = no video = falsey = false, 1 = video = truthy = true
         return data.items.length  
       } else {        
         return false
-      }    
+      }       
   }
 
   function updateInput(event) {
